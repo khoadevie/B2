604e306eb2310ce9d23cae6dbcdece5ccea94fff
Khoa Devie <khoado.devie@gmail.com>
2025-11-29 10:07:40 +0700

    Add files via upload

---
+++ b3.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE SalesDB;
+
+\c SalesDB
+
+CREATE SCHEMA sales;
+
+CREATE TABLE Customers
+(
+    customer_id SERIAL PRIMARY KEY,
+    first_name  VARCHAR(50)  NOT NULL,
+    last_name   VARCHAR(50)  NOT NULL,
+    email       VARCHAR(100) NOT NULL UNIQUE,
+    phone       VARCHAR(20)
+);
+
+CREATE TABLE Products
+(
+    product_id     SERIAL PRIMARY KEY,
+    product_name   VARCHAR(100)   NOT NULL,
+    price          NUMERIC(12, 2) NOT NULL,
+    stock_quantity INT            NOT NULL
+);
+
+CREATE TABLE Orders
+(
+    order_id    SERIAL PRIMARY KEY,
+    customer_id INT  NOT NULL REFERENCES Customers (customer_id),
+    order_date  DATE NOT NULL
+);
+
+CREATE TABLE OrderItems
+(
+    order_item_id SERIAL PRIMARY KEY,
+    order_id      INT NOT NULL REFERENCES Orders (order_id),
+    product_id    INT NOT NULL REFERENCES Products (product_id),
+    quantity      INT NOT NULL CHECK (quantity >= 1)
+);
+
+SELECT datname
+FROM pg_database;
+
+SELECT schema_name
+FROM information_schema.schemata;
+
+SELECT *
+FROM Customers
+
+SELECT *
+FROM Products
+
+SELECT * FROM Orders
+
+SELECT * FROM OrderItems
